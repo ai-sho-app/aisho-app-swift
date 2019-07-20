bb40228e9fa2a05785e9c17a2812de7c541644b2
shkwsk <ren7link@gmail.com>
2019-07-20 15:41:22 +0900

    Initial Commit

---
+++ aisho-app-swift/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  aisho-app-swift
+//
+//  Created by Sho Kawasaki on 2019/07/20.
+//  Copyright © 2019 Sho Kawasaki. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ aisho-app-swift/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  aisho-app-swift
+//
+//  Created by Sho Kawasaki on 2019/07/20.
+//  Copyright © 2019 Sho Kawasaki. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
